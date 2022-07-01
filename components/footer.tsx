5eed8997f1290d64533036930a606b2a8f25979e
MGNUR <rter.rter.1234321@gmail.com>
2022-07-01 20:49:49 +0900

    フッターおよびトップ画面に各種アイコンを追加

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,28 +1,41 @@
 import Container from './container'
-import { EXAMPLE_PATH } from '../lib/constants'
+import { IconContext } from 'react-icons'
+import { SiTwitter } from "react-icons/si"
+import { SiPixiv } from "react-icons/si"
+import { SiHatenabookmark } from "react-icons/si"
 
 const Footer = () => {
   return (
     <footer className="bg-neutral-50 border-t border-neutral-200">
       <Container>
-        <div className="py-28 flex flex-col lg:flex-row items-center">
-          <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Statically Generated with Next.js.
-          </h3>
-          <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
-            <a
-              href="https://nextjs.org/docs/basic-features/pages"
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
-            >
-              Read Documentation
-            </a>
-            <a
-              href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
-              className="mx-3 font-bold hover:underline"
-            >
-              View on GitHub
-            </a>
-          </div>
+        <div className="flex justify-end">
+          <a
+            href="https://www.pixiv.net/users/57155498"
+            className="p-2 mx-3"
+            target="_blank"
+          >
+            <IconContext.Provider value={{ size: '3em' }}>
+              <SiPixiv />
+            </IconContext.Provider>
+          </a>
+          <a
+            href="https://twitter.com/hanasome_sotai"
+            className="p-2 mx-3"
+            target="_blank"
+          >
+            <IconContext.Provider value={{ size: '3em' }}>
+              <SiTwitter />
+            </IconContext.Provider>
+          </a>
+          <a
+            href="https://priest-tea.hatenablog.com/"
+            className="p-2 mx-3"
+            target="_blank"
+          >
+            <IconContext.Provider value={{ size: '3em' }}>
+              <SiHatenabookmark />
+            </IconContext.Provider>
+          </a>
         </div>
       </Container>
     </footer>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
 import Container from '../components/container'
 import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
 import Image from 'next/image';
 import Layout from '../components/layout'
 import { getAllPosts } from '../lib/api'
 import Head from 'next/head'
 import { SITE_NAME } from '../lib/constants'
 import Post from '../types/post'
+import { IconContext } from 'react-icons'
+import { SiTwitter } from "react-icons/si"
+import { SiPixiv } from "react-icons/si"
+import { SiHatenabookmark } from "react-icons/si"
 
 type Props = {
   allPosts: Post[]
@@ -21,19 +24,50 @@ const Index = ({ allPosts }: Props) => {
           <title>{SITE_NAME}</title>
         </Head>
         <Container>
-          <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8 mb-1">
+        <div className="mu-2 flex items-end">
+          <h1 className="text-5xl md:text-8xl font-bold tracking-tighter md:pr-8 mb-1">
             牧場
           </h1>
+          <div className="flex ml-auto">
+            <a
+              href="https://www.pixiv.net/users/57155498"
+              className="p-2 mx-2 hover:placeholder-black"
+              target="_blank"
+            >
+              <IconContext.Provider value={{ color: '#ccc', size: '2em' }}>
+                <SiPixiv />
+              </IconContext.Provider>
+            </a>
+            <a
+              href="https://twitter.com/hanasome_sotai"
+              className="p-2 mx-2"
+              target="_blank"
+            >
+              <IconContext.Provider value={{ color: '#ccc', size: '2em' }}>
+                <SiTwitter />
+              </IconContext.Provider>
+            </a>
+            <a
+              href="https://priest-tea.hatenablog.com/"
+              className="p-2 mx-2"
+              target="_blank"
+            >
+              <IconContext.Provider value={{ color: '#ccc', size: '2em' }}>
+                <SiHatenabookmark />
+              </IconContext.Provider>
+            </a>
+          </div>
+          </div>
           <Image
             src="/assets/index/cover.jpg"
              height={1000}
              width={2000}
              alt="牧場"
           />
-          <p className="text-lg leading-relaxed m-4">
+          <p className="text-lg leading-relaxed m-4 mx-20">
             ほかの人たちがそのままで存在しているのを信じることが、愛である。
           </p>
-          <p className="text-lg leading-relaxed mb-8 text-right">
+          <p className="text-lg leading-relaxed mb-8 text-right mx-20">
             ――シモーユ・ヴェイヌ
           </p>
           {posts.length > 0 && <MoreStories posts={posts} />}
